098bc3f327fa1eafd279c251fab6c58b28cf852d
Brijraj888 <brijrajpandey88@gmail.com>
2022-11-12 12:36:13 +0530

    zohocrmapp

---
+++ src/main/java/com/blog/services/CommentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.blog.services;
+
+import com.blog.dto.CommentDto;
+import com.blog.entities.Comment;
+import com.blog.entities.Post;
+import com.blog.mapper.CommentMapper;
+import com.blog.repositories.CommentRepository;
+import com.blog.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+    public CommentServiceImpl(CommentRepository commentRepository,
+                              PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+    @Override
+    public void createComment(String postUrl, CommentDto commentDto) {
+        Post post = postRepository.findByUrl(postUrl).get();
+        Comment comment = CommentMapper.mapToComment(commentDto);
+        comment.setPost(post);
+        commentRepository.save(comment);
+    }
+}
+
